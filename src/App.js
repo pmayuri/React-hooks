bf16853e78049473a329096f3d75de1ac419004a
mayuri <pmayuri124@gmail.com>
2020-09-15 16:38:02 +0530

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      
+   {/*  <ClassCounter/> */}
+   <HookCounter/>
     </div>
   );
 }
 
-export default App;
+export default App
+
